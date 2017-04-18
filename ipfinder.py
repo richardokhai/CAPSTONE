1c6735d211372fa1a8e4b867749e1d151d26b136
richardokhai <richardokhai@hotmail.com>
2017-04-18 16:36:08 -0400

    Add files via upload

---
+++ ipfinder.py
@@ -0,0 +1,15 @@
+import os
+
+def get_ip_address(url):
+	command = "host " + url
+	process = os.popen(command)
+	results = str(process.read())
+	marker = results.find('has address') + 12
+	return results[marker:].splitlines()[0]
+
+
+
+print(get_ip_address('ferris.edu'))
+
+
+
